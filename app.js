c44c5c239fab29c44ca2b4055f5794c3c0594423
Conor O'Malley <conor4arms@gmail.com>
2023-04-20 18:59:04 -0400

    Actually gonna work this time watch

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/web-build')));
 
 // Add this catch-all route to serve index.html for all requests
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/web-build/index.html'));
 });
