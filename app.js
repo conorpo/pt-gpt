8c08b872a8e7fd6274db811fb3a11c356159226d
Conor O'Malley <conor4arms@gmail.com>
2023-04-20 19:30:02 -0400

    Added basic dev/production.

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const path = require('path');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 app.use(express.static(path.join(__dirname, 'frontend/web-build')));
 
 // Add this catch-all route to serve index.html for all requests
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/web-build/index.html'));
 });
+
+app.listen(PORT, () => { 
+  if(NODE_ENV === 'development') {
+    console.log(`Server is hosted at http://localhost:${PORT}`);
+  }else{
+    console.log(`Server is hosted at http://pt-gpt.com:${PORT}`);
+  }
+});
+
