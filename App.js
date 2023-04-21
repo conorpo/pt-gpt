8a2fa80dd67efee87cc8e941eefd9eb7fc28b158
ktorr <103144674+ktorr@users.noreply.github.com>
2023-04-20 20:00:55 -0400

    Update App.js

--- App.js
+++ App.js
@@ -24,18 +24,12 @@ import "./style.css";
 import UCFGym from './UCFGym.jpg';
 import polygon from './polygon-1.png';
 export default function App() {
-//export const Frame = () => {
   return (
     <div className={"frame-frame-wrapper"}>
       <div className={"frame-frame"}>
         <div className={"frame-overlap"}>
           <div className={"frame-rectangle"} />
           <img className={"frame-ucfgym"} src={UCFGym} />
-          <div className={"frame-hamburger"}>
-            <div className={"frame-rectangle-4"} />
-            <div className={"frame-rectangle-5"} />
-            <div className={"frame-rectangle-6"} />
-          </div>
         </div>
         <div className={"frame-message"}>
           <div className={"frame-overlap-group"}>
@@ -44,7 +38,62 @@ export default function App() {
             <h1 className={"frame-text-wrapper"}>Find the perfect workouts to meet your goals!</h1>
           </div>
         </div>
+        <button type="button" className={"frame-div"}>Enter Data</button>
+        <button type="button" className={"frame-text-wrapper-2"}>Chat with Personal Trainer</button>
       </div>
     </div>
   );
 };
+
+function Data() {
+  return (
+    <div className={"data-data-wrapper"}>
+      <div className={"data-data"}>
+        <div className={"data-back-arrow"}>
+          <div className={"data-rectangle"} />
+          <div className={"data-rectangle-3"} />
+        </div>
+        <div className={"data-height"}>
+          <div className={"data-text-wrapper"}>Height</div>
+          <input className={"data-feet"} />
+          <div className={"data-div"}>ft.</div>
+          <input className={"data-in"} />
+          <div className={"data-text-wrapper-2"}>in.</div>
+        </div>
+        <div className={"data-weight"}>
+          <div className={"data-text-wrapper-3"}>Weight</div>
+          <input className={"data-input"} />
+          <div className={"data-text-wrapper-4"}>lbs.</div>
+        </div>
+        <div className={"data-gender"}>
+          <div className={"data-p"}>Gender (enter M or F)</div>
+          <input className={"data-input"} />
+        </div>
+        <h1 className={"data-h-1"}>Data</h1>
+      </div>
+    </div>
+  );
+}
+
+function Chat () {
+  return (
+    <div className={"PT-PT-wrapper"}>
+      <div className={"PT-PT"}>
+        <div className={"PT-overlap"}>
+          <div className={"PT-heading"}>
+            <div className={"PT-overlap-group"}>
+              <div className={"PT-overlap-group1"}>
+                <div className={"PT-text-wrapper"}>PT</div>
+              </div>
+              <div className={"PT-div"}>Personal Trainer</div>
+            </div>
+          </div>
+          <div className={PT-back-arrow}>
+            <div className={"PT-rectangle"} />
+            <div className={"PT-rectangle-3"} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
